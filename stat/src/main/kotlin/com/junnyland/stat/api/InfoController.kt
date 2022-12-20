34f32218731501129396b0cc3c47be39942b866c
JunnyJun <chbe5082@naver.com>
2022-12-20 23:00:19 +0900

    set

--- stat/src/main/kotlin/com/junnyland/stat/api/InfoController.kt
+++ stat/src/main/kotlin/com/junnyland/stat/api/InfoController.kt
@@ -1,21 +1,37 @@
 package com.junnyland.stat.api
 
 import com.junnyland.stat.client.ParserInfo
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
+import org.springframework.core.io.ResourceLoader
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 
-@Controller
+@RestController
 @RequestMapping("/api")
 class InfoController(
-    private val parserInfo: ParserInfo
+    private val parserInfo: ParserInfo,
+    private val resourceLoader: ResourceLoader
 ) {
+
+    private val DEFAULT = resourceLoader.getResource("classpath:static/default.svg").file;
+
     @GetMapping("/info")
-    fun info(@RequestParam userId : String, model: Model): String {
-        model.addAttribute("info", parserInfo.call(userId))
-        return "info"
+    fun info(@RequestParam userId: String): ResponseEntity<String> {
+        val call = parserInfo.call(userId)
+        val readLines = DEFAULT.readLines()
+            .asSequence()
+            .map { it.replace("{{submit}}", call.submit) }
+            .map { it.replace("{{resolved}}", call.solved) }
+            .map { it.replace("{{failed}}", call.fail) }
+            .map { it.replace("{{my}}", userId) }
+            .joinToString("\n")
+
+        return ResponseEntity.ok()
+            .header("Content-Type", "image/svg+xml")
+            .header("Connection", "keep-alive")
+            .body(readLines);
     }
 }
