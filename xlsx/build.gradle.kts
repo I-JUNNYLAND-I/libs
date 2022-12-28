cc1c863bbca10b442b17928dd52d3e9817b49fd3
junnyJun <chbe5082@naver.com>
2022-12-28 15:58:46 +0900

    feat(dependency) : junit

--- xlsx/build.gradle.kts
+++ xlsx/build.gradle.kts
@@ -14,7 +14,10 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.apache.poi:poi:5.2.3")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
 }
 
 tasks.test {
