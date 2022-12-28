b2becc3fd6127779ff0cb2bb2ac599267e3f6ef6
junnyJun <chbe5082@naver.com>
2022-12-28 16:00:50 +0900

    feat(build) : remove unused

--- xlsx/src/main/kotlin/junny/land/xlsx/Sample.kt
+++
@@ -1,9 +0,0 @@
-package junny.land.xlsx
-
-import junny.land.xlsx.annotation.Column
-
-class Sample(
-    @Column(name = "이름") var name: String,
-    @Column(name = "나이") var age: Int,
-    var noAnno: Int
-)
