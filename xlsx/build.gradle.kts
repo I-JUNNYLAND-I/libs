d6ac63eaeb0ce16e7b18cbf3eb940f415618d8d5
junnyJun <chbe5082@naver.com>
2022-12-28 16:44:26 +0900

    feat(build) : dependency spring

--- xlsx/build.gradle.kts
+++ xlsx/build.gradle.kts
@@ -1,21 +1,28 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("org.springframework.boot") version "3.0.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("plugin.spring") version "1.7.22"
     kotlin("jvm") version "1.7.22"
 }
 
 group = "junny.land"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    implementation("org.apache.poi:poi:5.2.3")
+    // spring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.apache.poi:poi:5.2.3")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
 }
