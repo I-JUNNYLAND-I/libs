feded8da8edf356189d772d1e6db13bbcbdb18b0
junnyJun <chbe5082@naver.com>
2023-01-02 16:31:43 +0900

    java to kotlin

--- changer/src/main/kotlin/junny/land/xlsx/datas/FieldDatas.kt
+++ changer/src/main/kotlin/junny/land/xlsx/datas/FieldDatas.kt
@@ -4,7 +4,6 @@ package junny.land.xlsx.datas
 data class FieldDatas(val data:List<String>) {
     val csv: String
         get() = data.joinToString(",")
-
     val plain:String
         get() = "|${data.joinToString("|") { it.padEnd(20) }}|"
 }
