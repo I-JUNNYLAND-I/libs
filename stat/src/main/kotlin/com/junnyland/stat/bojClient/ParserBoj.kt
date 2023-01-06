f29d54b8b6203c910f11c72483c1bad08b900e44
junnyJun <chbe5082@naver.com>
2023-01-06 12:48:40 +0900

    parse dependency

---
+++ stat/src/main/kotlin/com/junnyland/stat/bojClient/ParserBoj.kt
@@ -0,0 +1,40 @@
+package com.junnyland.stat.bojClient
+
+import com.junnyland.stat.config.ApiClient
+import com.junnyland.stat.converter.Converter
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+interface ParserBoj {
+    fun call(userId:String): Boj
+
+    @Component
+    class Baekjoon(
+        @Value("\${baekjoon.url}") private val url: String,
+        private val apiClient: ApiClient
+    ) : ParserBoj {
+        override fun call(userId: String): Boj {
+            val info = url+userId
+
+            val get = Jsoup.connect(info).get()
+
+            val myData = get.getElementById("statics")!!.allElements.eachText()[0].split(" ")
+
+            return Boj(
+                grade = myData[1],
+                submit = myData[11],
+                solved = myData[13],
+                fail = myData[18],
+                badge = findBadge(get),
+                myPage = info
+            )
+        }
+
+        private fun findBadge(get: Document) = get.getElementsByClass("solvedac-tier").attr("src")
+                .let { apiClient.get(it).orEmpty() }
+                .let { Converter.convert(it) }
+
+    }
+}
--- stat/src/test/kotlin/com/junnyland/stat/service/ParserBojTest.kt
+++ stat/src/test/kotlin/com/junnyland/stat/service/ParserBojTest.kt
@@ -1,12 +1,15 @@
 package com.junnyland.stat.service
 
+import com.junnyland.stat.bojClient.ParserBoj
+import com.junnyland.stat.config.ApiClient
+import com.junnyland.stat.config.ApiConfig
 import org.junit.jupiter.api.Test
 
 class ParserBojTest{
 
     @Test
     fun `test`(){
-        val baekjoon = ParserBoj.Baekjoon("https://www.acmicpc.net/user/")
+        val baekjoon = ParserBoj.Baekjoon("https://www.acmicpc.net/user/", ApiClient(ApiConfig().webClient()))
         val call = baekjoon.call("wjwan0")
 
         println("call = ${call}")
