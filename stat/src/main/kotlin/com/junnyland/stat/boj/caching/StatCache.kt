e1e3356010a4d622aa896cbc60dd5251f0d77949
JunnyJun <chbe5082@naver.com>
2023-01-09 16:18:32 +0900

    refactor

--- stat/src/main/kotlin/com/junnyland/stat/boj/caching/StatCache.kt
+++ stat/src/main/kotlin/com/junnyland/stat/boj/caching/StatCache.kt
@@ -1,28 +1,34 @@
 package com.junnyland.stat.boj.caching
 
 import com.junnyland.stat.boj.bojClient.Boj
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
-import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
-interface StatCache{
+interface StatCache {
     fun exists(userId: String): Boolean
     fun find(userId: String): Boj
-    fun save(boj: Boj, userId:String): Stat?
-    fun finaAll(): Any
+
+    fun save(boj: Boj, userId: String)
+    fun findAll(): Iterable<Stat>
 
     @Repository
     class StatCacheImpl(
         private val statCacheRepository: StatCacheRepository
     ) : StatCache {
-
-        override fun exists(key:String)= statCacheRepository.existsById(key)
-        override fun find(key:String)= statCacheRepository.findByIdOrNull(key)
-            ?.let { it.toDomain() }
+        override fun exists(key: String) = statCacheRepository.existsById(key)
+        override fun find(key: String) = statCacheRepository.findByIdOrNull(key)
+            ?.run(Stat::toDomain)
             ?: throw Exception("Not Found")
-        @Transactional
-        override fun save(boj: Boj, userId:String) = statCacheRepository.save(Stat(boj,userId))
-        override fun finaAll() = statCacheRepository.findAll()
+
+        override fun save(boj: Boj, userId: String) {
+            CoroutineScope(Dispatchers.Default).launch {
+                statCacheRepository.save(Stat(boj, userId))
+            }
+        }
+
+        override fun findAll(): Iterable<Stat> = statCacheRepository.findAll()
     }
 }
