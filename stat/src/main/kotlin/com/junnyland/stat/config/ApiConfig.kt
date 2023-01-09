66640ed39d53be9ed0b281ee6f87e690b1237bbb
JunnyJun <chbe5082@naver.com>
2023-01-09 16:00:32 +0900

    refactor

--- stat/src/main/kotlin/com/junnyland/stat/config/ApiConfig.kt
+++ stat/src/main/kotlin/com/junnyland/stat/config/ApiConfig.kt
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClient.Builder
 import reactor.netty.http.client.HttpClient
 import java.time.Duration
 import java.util.concurrent.TimeUnit
@@ -21,23 +22,29 @@ fun Builder.initHeaders() = this
 @Configuration
 class ApiConfig {
     @Bean
-    fun webClient(): WebClient {
-        val strategies = ExchangeStrategies.builder()
+    fun webClient() = WebClient.builder()
+        .exchangeStrategies(createStrategies())
+        .clientConnector(ReactorClientHttpConnector(createHttpClient()))
+        .initHeaders()
+        .build()
+
+    private fun createStrategies(): ExchangeStrategies =
+        ExchangeStrategies.builder()
             .codecs { it.defaultCodecs().maxInMemorySize(MEMORY_BUFFER) }
             .build()
 
-        val httpClient = HttpClient.create()
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNECTION_TIMEOUT)
-            .responseTimeout(Duration.ofMillis(READ_TIMEOUT.toLong()))
-            .doOnConnected { it
-                .addHandlerLast(ReadTimeoutHandler(READ_TIMEOUT.toLong(), TimeUnit.MILLISECONDS))
-                .addHandlerLast(WriteTimeoutHandler(WRITE_TIMEOUT.toLong(), TimeUnit.MILLISECONDS))
-            }
-        return WebClient.builder()
-            .exchangeStrategies(strategies)
-            .clientConnector(ReactorClientHttpConnector(httpClient))
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-            .build()
+    private fun createHttpClient(): HttpClient = HttpClient.create()
+        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNECTION_TIMEOUT)
+        .responseTimeout(Duration.ofMillis(READ_TIMEOUT))
+        .doOnConnected { it
+                .addHandlerLast(ReadTimeoutHandler(READ_TIMEOUT, TimeUnit.MILLISECONDS))
+                .addHandlerLast(WriteTimeoutHandler(WRITE_TIMEOUT, TimeUnit.MILLISECONDS))
+        }
+
+    companion object {
+        private val MEMORY_BUFFER = 100 * 1_024 * 1_024
+        private val CONNECTION_TIMEOUT = 30_000
+        private val READ_TIMEOUT = 30_000L
+        private val WRITE_TIMEOUT = 30_000L
     }
 }
