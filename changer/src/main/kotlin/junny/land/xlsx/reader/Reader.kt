01ba868c867c52b19b1ec7cc0590194619a968ac
JunnyJun <chbe5082@naver.com>
2023-01-31 01:14:48 +0900

    create Reader

--- changer/src/main/kotlin/junny/land/xlsx/reader/Reader.kt
+++ changer/src/main/kotlin/junny/land/xlsx/reader/Reader.kt
@@ -1,4 +1,29 @@
 package junny.land.xlsx.reader
 
-class Reader() {
+import junny.land.xlsx.reader.model.DataType
+import java.io.File
+import java.io.OutputStream
+import java.nio.file.Path
+
+class Reader<T>(val response: Class<T>){
+    lateinit var type: DataType
+    fun create(): DataSelector<T> = DataSelector(Reader(response))
+
+    companion object {
+        fun <T> create(response: Class<T>): DataSelector<T> = DataSelector(Reader(response))
+    }
+
+    fun response(): Class<T> = response
+
+    fun type(type: File) {
+        this.type = DataType.file(type)
+    }
+
+    fun type(type: OutputStream) {
+        this.type = DataType.outputStream(type)
+    }
+
+    fun type(type: Path) {
+        this.type = DataType.path(type)
+    }
 }
