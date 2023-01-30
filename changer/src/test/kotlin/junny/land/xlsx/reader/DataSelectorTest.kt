96a9ee73493defb95e3cec56dcfdd61f34738a0f
JunnyJun <chbe5082@naver.com>
2023-01-31 01:19:58 +0900

    byte array

--- changer/src/test/kotlin/junny/land/xlsx/reader/DataSelectorTest.kt
+++ changer/src/test/kotlin/junny/land/xlsx/reader/DataSelectorTest.kt
@@ -4,28 +4,27 @@ import junny.land.xlsx.samples.SampleJC
 import junny.land.xlsx.samples.SampleKt
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
 import java.io.File
 
 class DataSelectorTest {
     @Test
     fun fileTest() {
         val reader = Reader(SampleJC::class.java).create()
-            .type(File("test.xlsx"))
+            .type(File("test.txt"))
 
         reader.root.type.fileValue?.let {
-            assertEquals("test.xlsx", it.name)
+            assertEquals("test.txt", it.name)
         }
     }
 
     @Test
     fun test2() {
         val reader = Reader.create(SampleKt::class.java)
-            .type(File("test.xlsx").outputStream())
+            .type(File("test.txt").outputStream())
 
         reader.root.type.outputStreamValue?.let {
-             ByteArray(1024)
-                .also { bytes -> it.write(bytes) }
-                .let { String(it) }
+            String(ByteArrayOutputStream().also { bytes -> it.write(bytes.toByteArray()) }.toByteArray())
                  .run { println(this) }
         }
     }
@@ -33,10 +32,10 @@ class DataSelectorTest {
     @Test
     fun test3() {
         val reader = Reader.create(SampleKt::class.java)
-            .type(File("test.xlsx").toPath())
+            .type(File("test.txt").toPath())
 
         reader.root.type.pathValue?.let {
-            assertEquals("test.xlsx", it.toString())
+            assertEquals("test.txt", it.toString())
         }
     }
 }
