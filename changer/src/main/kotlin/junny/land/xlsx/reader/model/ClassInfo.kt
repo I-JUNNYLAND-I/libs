eef276c3ed1316f61af9e47d355187660299ebeb
JunnyJun <chbe5082@naver.com>
2023-02-09 23:17:57 +0900

    class info

--- changer/src/main/kotlin/junny/land/xlsx/reader/model/ClassInfo.kt
+++ changer/src/main/kotlin/junny/land/xlsx/reader/model/ClassInfo.kt
@@ -4,24 +4,12 @@ data class ClassInfo(
     var headerRow: Int = 0,
     var dataRow: Int = 1,
 ) {
-    private var headerList: MutableList<String> = mutableListOf()
-    private var mappingIndexByFieldList: MutableList<Int> = mutableListOf()
+    private var fieldList: MutableList<String> = mutableListOf()
 
-    fun headerList(clazz: Class<*>) = clazz.declaredFields
-        .forEach { element -> element.annotations
-                .first { it is Header }
-                .also { headerList.add((it as Header).value) }
+    fun fieldList(response: Class<*>) = response
+        .declaredFields
+        .forEach {
+            fieldList.add(it.name)
         }
-
-    // 엑셀 헤더에 선언된 값 순서와 맞춰주기 위함
-    fun mappingIndexByFieldList(fields: List<String>) = fields
-        .forEach { element ->
-            mappingIndexByFieldList.add(headerList.indexOf(element))
-        }
-
-
-    fun fieldWithData(index: Int, data: String) =
-        mapOf(headerList[mappingIndexByFieldList[index]] to data)
-
 }
 
--- changer/src/test/kotlin/junny/land/xlsx/reader/model/ClassInfoTest.kt
+++ changer/src/test/kotlin/junny/land/xlsx/reader/model/ClassInfoTest.kt
@@ -9,24 +9,16 @@ class ClassInfoTest {
     @Test
     fun findHeaderTest() {
         val classInfo = ClassInfo()
-            .headerList(SampleHeader::class.java)
+            .fieldList(SampleHeader::class.java)
 
         println("javaClass = ${javaClass}")
     }
 
     @Test
     fun findHeaderIndexTest() {
-        val classInfo = ClassInfo()
-            .also { it.headerList(SampleHeader::class.java) }
-            .also { it.mappingIndexByFieldList(listOf("data2", "data3", "data1")) }
     }
 
     @Test
     fun findHeaderExtract() {
-        ClassInfo()
-            .also { it.headerList(SampleHeader::class.java) }
-            .also { it.mappingIndexByFieldList(listOf("data2", "data3", "data1")) }
-            .fieldWithData(1, "test-data1")
-            .apply { assertEquals(this["data3"], "test-data1") }
     }
 }
